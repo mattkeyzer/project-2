254fb97e89eca3a96fc15e9a41bbdfc9f942d78a
mattkeyzer <105663113+mattkeyzer@users.noreply.github.com>
2022-07-18 19:09:04 -0500

    Add files via upload

---
+++ project 2/javascript/change.js
@@ -0,0 +1,83 @@
+function change01() {
+    document.getElementById("large-image").src = "images/family-photo.jpeg";
+}
+
+function change02() {
+    document.getElementById("large-image").src = "images/family-paul.jpeg";
+}
+
+function change03() {
+    document.getElementById("large-image").src = "images/family-vacation.jpeg"
+}
+
+function change04() {
+    document.getElementById("large-image").src = "../images/mike-outside.jpeg";
+}
+
+function change05() {
+    document.getElementById("large-image").src = "../images/mike-horseshoe.jpeg";
+}
+
+function change06() {
+    document.getElementById("large-image").src = "../images/mike-funny.jpeg"
+}
+
+function change07() {
+    document.getElementById("large-image").src = "../images/mike-run.jpeg";
+}
+
+function change08() {
+    document.getElementById("large-image").src = "../images/mike-thumbs.jpeg";
+}
+
+function change09() {
+    document.getElementById("large-image").src = "../images/mike-vacation.jpeg"
+}
+
+function change10() {
+    document.getElementById("large-image").src = "../images/debi-solo.jpeg";
+}
+
+function change11() {
+    document.getElementById("large-image").src = "../images/debi-hulk.jpeg";
+}
+
+function change12() {
+    document.getElementById("large-image").src = "../images/debi-arizona.jpeg"
+}
+
+function change13() {
+    document.getElementById("large-image").src = "../images/debi-jersey.jpeg";
+}
+
+function change14() {
+    document.getElementById("large-image").src = "../images/debi-vacation.jpeg";
+}
+
+function change15() {
+    document.getElementById("large-image").src = "../images/debi-mike.jpeg"
+}
+
+function change16() {
+    document.getElementById("large-image").src = "../images/matt-banquet.jpeg";
+}
+
+function change17() {
+    document.getElementById("large-image").src = "../images/matt-fish.jpeg";
+}
+
+function change18() {
+    document.getElementById("large-image").src = "../images/matt-vacation.jpeg"
+}
+
+function change19() {
+    document.getElementById("large-image").src = "../images/amy-piper.jpeg";
+}
+
+function change20() {
+    document.getElementById("large-image").src = "../images/amy-kitty.jpeg";
+}
+
+function change21() {
+    document.getElementById("large-image").src = "../images/amy-chicago.jpeg"
+}
